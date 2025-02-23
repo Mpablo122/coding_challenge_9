a7e7abf1e1bd62156346fc0f3082c1b07097c331
Mark Francis Pablo <markfrancispablojr@gmail.com>
2025-02-22 19:38:57 -0500

    Task 2 Created manager class with inheritance

--- cc_9.js
+++ cc_9.js
@@ -20,3 +20,25 @@ const emp1 = new employee("Alice Johnson", 101, "Sales", 5000);
 console.log(emp1.getDetails());
 console.log(emp1.calculateAnnualSalary());
 //Expected output: 60000
+
+//Task 2 Created manager class with inheritance
+
+class manager {
+    constructor (name, id, department, salary,teamSize){
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+        this.teamSize = teamSize;
+
+    }
+    getDetails(){
+        return `Manager: ${this.name}, ID: ${this.id},Department: ${this.department}, Salary: $${this.salary}`
+    }
+    calculateBonus(){
+        return this.salary * 12 *0.1;
+    }
+}
+const mgr1 = new manager("John Smith", 201, "IT", 8000,5);
+console.log(mgr1.getDetails());
+console.log(mgr1.calculateBonus());
